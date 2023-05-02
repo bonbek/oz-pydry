b8df182d874d26cd70daec50f61103eaabd891f6
Bonbek <nantistress@gmail.com>
2023-05-02 11:47:17 +0200

    Streamlit - merge RR streamlit_v5

--- streamlit_app/Introduction.py
+++ streamlit_app/Introduction.py
@@ -23,18 +23,14 @@ st.markdown(
     """
     
     "Quel temps fera-t-il demain ?" est peut être l'une des questions les plus fréquentes et répandue depuis que l'humanité projète des plans.
-    Bien avant l'arrivée des systèmes informatiques, certains étudiaient les différentes conditions climatique pour établir des prédictions.
-
-    Depuis l'instalation de sondes et l'utilisations de systèmes informatiques, la précision de ces prédictions météorologique est sensiblement améliorée.
+    Bien avant l'arrivée des systèmes informatiques, certains étudiaient les différentes conditions climatiques pour établir des prédictions.
+    Depuis l'instalation de sondes et l'utilisations de systèmes informatiques, la précision de ces prédictions météorologique s'est sensiblement améliorée.
     
     L'Australie est un pays avec des zones arides et semi-arides importantes, les incendies de forêts y sont fréquents, surtout pendant les périodes de sécheresse. La pluie est vitale pour la survie de la faune et de la flore et est essentielle pour l'agriculture, l'un des piliers économiques de l'Australie.
-
     Bien que les précipitations soient souvent faibles, tout au long de son histoire, l'Australie à subit des inondations aux conséquences dévastatrices. La prévision de la pluie peut permettre de prendre des mesures pour minimiser les dommages causés par les inondations.
-
-    En somme, la prévision de la pluie en Australie est cruciale pour la sécurité et le bien-être des êtres humains, de la faune, la flore et de l'économie.
-
-    Nous souhaitons donc contribuer à la précision des prévisions météorologique en Australie.
-    Pour cela, nous utiliserons leurs données recensées avec nos modèles utilisant les outils de machine learning et de deep learning.
+    En somme, la prévision de la pluie en Australie est cruciale pour la sécurité et le bien-être des êtres humains, de la faune, de la flore et de l'économie.
+    Nous souhaitons donc contribuer à la précision des prévisions météorologiques en Australie.
+    Pour cela, nous utiliserons leurs données recensées en Australie avec nos modèles de machine learning et de deep learning.
     
     """
 )
--- streamlit_app/pages/05_Perspectives.py
+++ streamlit_app/pages/05_Perspectives.py
@@ -24,7 +24,6 @@ st.markdown(
     appliquant des comparaisons selon un preprocessing spécifique (avec ou sans PCA, avec ou sans les lignes contenant des NaNs, Avec les 3 climats, les 9 sous-climats ou l’ensemble d'entraînement, les différents modes d’équilibrage des classes. 
     Mais les résultats de précision et de recall avec les modèles de machine learning n’ont pas été suffisamment concluants pour nous. Nous avons donc appliqué un modèle de Deep Learning, les résultats obtenus sont meilleurs que les modèles de machine learning,  notamment sur le recall de la classe 1. 
     On se rapproche des résultats des modèles de prédiction réels utilisés en Australie sur la période étudiée.
-
     """
 )
 
@@ -35,22 +34,14 @@ st.subheader(
 )
 st.markdown(
     """
-    Avec du délais supplémentaire nous aurions utilisé les données temporelle pour réalisé des Manque de donnée par rapport au autres modèle live,
-    manque de temps pour intégrer une valeur temporele pour faire des séries temporelles
-    Problématique concentrée uniquement sur une problématique binaire
-    Déception sur les précisions obtenues par rapport aux modèles météorologiques réel
+    Avec du délais supplémentaire nous aurions utilisé les données temporelles pour réalisé un modèle RNN
+    Nous aurions consolidé le dataset avec des nouvelles variables issue de la topographie des lieu, à l'image du modèle AROME (modèle Français)
+    En fin de compte nous retenons une certaine déception sur les précisions obtenues qui ne sont pas meilleurs que les modèles météorologiques réel, alors que nous somme focalisé sur qu'une seule variable cible.
     
-    En France, le modèle AROME (Application of Research to Operations at Mesoscale) est le modèle qui s’identifie comme une évolution à venir de notre modèle. En plus des données classiques que l’on retrouve dans notre base de données,
-    il prend des éléments issus de la topographie du lieu (forêt, cours d’eau, ville, neige, bétonisation…etc), ainsi qu’une meilleure compréhension des nuages, qui ont un effet sur la pluviométrie.
-
-
-        
-    Rappelons que le 18 Août 2022, une tempête en Corse a causé 5 décès, ainsi que de gros dégâts matériels. Pourtant, la veille, Météo France avait classé l’ile en vigilance jaune. 
+                
+    Rappelons que le 18 Août 2022, une tempête en Corse a causé 5 décès, ainsi que de gros dégâts matériels. Pourtant, la veille, Météo France avait classé la Corse en vigilance jaune. 
     Bien que les modèles ont gagné en précisions sur les dernières décennies, il y encore beaucoup de marge à gagner pour s’approcher des 100% de précision mais aussi gagner dans la portée de prédiction à J+14.
     Les conditions climatiques à venir vont avoir de lourdes conséquences pour l'ensemble de la population mondiale, les bonnes prédictions climatiques n’ont jamais été aussi importantes pour anticiper les catastrophes naturelles à venir.
-
-
     
-
     """
 )
