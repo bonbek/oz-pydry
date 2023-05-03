8efaaf18921b707d6927775e930a6a90c2079594
Bonbek <nantistress@gmail.com>
2023-05-03 12:13:45 +0200

    Streamlit - update Perspectives page.

--- streamlit_app/pages/05_Perspectives.py
+++ streamlit_app/pages/05_Perspectives.py
@@ -2,15 +2,20 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import utils.layout as layout
+import os
+import sys
+sys.path.insert(0, os.path.abspath("../lib"))
+from ozpydry.metrics import ClfReport
 
 layout.base_styles()
 layout.show_members()
 
-title = "Regard critique et perspectives"
-sidebar_name = "Regard critique et perspectives"
+st.title(
+    """
+    Autocritique et perspectives
+    """)
 
-
-st.title(title)
+st.image('./assets/comic-strip.png')
 
 st.subheader(
     """
@@ -19,29 +24,60 @@ st.subheader(
 )
 st.markdown(
     """
-    Après avoir étudié les caractéristiques de notre base de données lors de l’exploration, nous avons établi plusieurs hypothèses afin de créer le modèle le plus performant en précision. 
-    Une fois notre premier modèle naïf créé nous avons comparé avec différents modèles de machine learning tout en
-    appliquant des comparaisons selon un preprocessing spécifique (avec ou sans PCA, avec ou sans les lignes contenant des NaNs, Avec les 3 climats, les 9 sous-climats ou l’ensemble d'entraînement, les différents modes d’équilibrage des classes. 
-    Mais les résultats de précision et de recall avec les modèles de machine learning n’ont pas été suffisamment concluants pour nous. Nous avons donc appliqué un modèle de Deep Learning, les résultats obtenus sont meilleurs que les modèles de machine learning,  notamment sur le recall de la classe 1. 
-    On se rapproche des résultats des modèles de prédiction réels utilisés en Australie sur la période étudiée.
+    Après avoir étudié les caractéristiques de notre base de données, nous avons établi plusieurs hypothèses afin de créer un modèle baseline.
+    Nous l'avons ensuite comparé avec d'autres modèles de machine learning tout en appliquant des preprocessings spécifiques: avec ou sans PCA, avec ou sans les valeurs manquantes, avec les 3 climats, les 9 sous-climats ou l’ensemble d'entraînement, l’équilibrage des classes...
+
+    Les performances de ces modèles étant peu concluantes, nous avons donc appliqué un modèle de Deep Learning qui s'est révélé plus homogène dans ses résultats, notamment sur le _recall_ et la _precision_ de la classe minoritaire _(Yes)_.
+
+    Nous sommes arrivés proches des résultats obtenus par les modèles de prédiction utilisés en Australie sur la période étudiée :smiley:.
+    
+    En fin de compte nous retenons une certaine déception sur les performances obtenues, alors que nous sommes focalisés sur une seule variable cible binaire.
     """
 )
 
 st.subheader(
     """
-    Amélioration et Perspectives
+    Pistes d'amélioration
     """
 )
+
 st.markdown(
     """
-    Avec du délais supplémentaire nous aurions utilisé les données temporelles pour réalisé un modèle RNN
-    Nous aurions consolidé le dataset avec des nouvelles variables issue de la topographie des lieu, à l'image du modèle AROME (modèle Français)
-    En fin de compte nous retenons une certaine déception sur les précisions obtenues qui ne sont pas meilleurs que les modèles météorologiques réel, alors que nous somme focalisé sur qu'une seule variable cible.
+    Avec du temps supplémentaire, nous aurions étudié plus en détail l'aspect temporel des variables, en effet, à mesure de notre avancement nous avons remarqué des "comportements saisonniers" sur certains de nos modèles (additionnés au climats).
+    """
+)
+
+with st.expander("Aperçu dans le temps de predictions projetées sur les variables principales"):
+    st.image('./assets/plot-preds-seasonal-features.png')
+    report = ClfReport('./assets/per-season-climate-lr.json')
+    st.write(report.to_markdown(compact=True, tone=True, title='Per season & climate model _(LogReg.)_'), unsafe_allow_html=True)
+    st.write("""
+    <style>
+    .rt tr > td:first-child {
+        width: 30%;
+    }
+    </style>
+    """,unsafe_allow_html=True)
+
+st.markdown(
+    """
+    Nous aurions aussi consolidé le dataset avec des nouvelles variables issue de la topographie des lieu, à l'image du modèle AROME (modèle Français). 
     
-                
-    Rappelons que le 18 Août 2022, une tempête en Corse a causé 5 décès, ainsi que de gros dégâts matériels. Pourtant, la veille, Météo France avait classé la Corse en vigilance jaune. 
-    Bien que les modèles ont gagné en précisions sur les dernières décennies, il y encore beaucoup de marge à gagner pour s’approcher des 100% de précision mais aussi gagner dans la portée de prédiction à J+14.
+    Pour ces axes de recherche, des modèles de type RNN (ou du moins une combinaison avec) nous semblent une piste adapté.
+    """
+)
+
+st.subheader(
+    """
+    Rappel des enjeux
+    """
+)
+
+st.markdown(
+    """
+    Le 18 Août 2022, une tempête en Corse a causé 5 décès, ainsi que de gros dégâts matériels. Pourtant, la veille, Météo France avait classé la Corse en vigilance jaune. 
+    Bien que les modèles se soient améliorés ces dernières décennies, il y encore une grande marge pour s’approcher des 100% de précision mais aussi gagner dans la portée à J+14.
+
     Les conditions climatiques à venir vont avoir de lourdes conséquences pour l'ensemble de la population mondiale, les bonnes prédictions climatiques n’ont jamais été aussi importantes pour anticiper les catastrophes naturelles à venir.
-    
     """
 )
